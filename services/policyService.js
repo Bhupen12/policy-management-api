f67c74884fe18dbcb34540fee3447de2633f69ad
alien <bhupensahoo200127@gmail.com>
2026-01-11 21:19:03 +0530

    take firstName into considaration and return required data

--- services/policyService.js
+++ services/policyService.js
@@ -1,19 +1,18 @@
 import { PolicyModel, UserModel } from "../models/index.js";
 
 export const findPoliciesByUsername = async (username) => {
-  const user = await UserModel.findOne({ email: username });
+  const user = await UserModel.findOne({
+    $or: [{ email: username }, { firstName: username }]
+  });
 
-  if (!user) {
-    return null;
-  }
+  if (!user) return [];
 
-  const policies = await PolicyModel.find({ user: user._id })
-    .populate('category')
-    .populate('carrier')
-    .populate('user');
+  return PolicyModel.find({ user: user._id })
+    .populate('category', 'name')
+    .populate('carrier', 'name')
+    .populate('user', 'firstName email');
+};
 
-  return policies;
-}
 
 export const aggregatePoliciesByUser = async () => {
   const today = new Date();
@@ -25,7 +24,16 @@ export const aggregatePoliciesByUser = async () => {
         totalPolicies: { $sum: 1 },
         activePolicies: {
           $sum: {
-            $cond: [{ $gte: ["$endDate", today] }, 1, 0]
+            $cond: [
+              {
+                $and: [
+                  { $lte: ["$startDate", today] },
+                  { $gte: ["$endDate", today] }
+                ]
+              },
+              1,
+              0
+            ]
           }
         },
         expiredPolicies: {
