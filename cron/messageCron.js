24df4712a7dbdecca1aaf12244fd4603f4580bf3
alien <bhupensahoo200127@gmail.com>
2026-01-11 22:14:08 +0530

    move message process logic to service

--- cron/messageCron.js
+++ cron/messageCron.js
@@ -4,25 +4,8 @@ import { ScheduledMessageModel, MessageModel } from "../models/index.js";
 export const startMessageCron = () => {
   cron.schedule("*/30 * * * * *", async () => {
     try {
-      const now = new Date();
-
-      const pendingMessages = await ScheduledMessageModel.find({
-        scheduledAt: { $lte: now },
-        status: "PENDING"
-      });
-
-      if (!pendingMessages.length) return;
-
-      for (const job of pendingMessages) {
-        await MessageModel.create({
-          message: job.message,
-          deliveredAt: new Date()
-        });
-
-        job.status = "PROCESSED";
-        await job.save();
-        console.log(`Processed scheduled message ID: ${job._id}`);
-      }
+      await processScheduledMessages();
+      console.log("Scheduled messages processed");
     } catch (error) {
       console.error("Cron Job Error:", error);
     }
--- services/messageService.js
+++ services/messageService.js
@@ -1,4 +1,4 @@
-import { ScheduledMessageModel } from "../models/index.js";
+import { MessageModel, ScheduledMessageModel } from "../models/index.js";
 
 export const scheduleMessage = async (message, scheduledAt) => {
   return await ScheduledMessageModel.create({
@@ -7,3 +7,24 @@ export const scheduleMessage = async (message, scheduledAt) => {
     status: "PENDING"
   });
 }
+
+export const processScheduledMessages = async () => {
+  const now = new Date();
+
+  const pendingMessages = await ScheduledMessageModel.find({
+    scheduledAt: { $lte: now },
+    status: "PENDING"
+  });
+
+  if (!pendingMessages.length) return;
+
+  for (const job of pendingMessages) {
+    await MessageModel.create({
+      message: job.message,
+      deliveredAt: new Date()
+    });
+
+    job.status = "PROCESSED";
+    await job.save();
+  }
+};
