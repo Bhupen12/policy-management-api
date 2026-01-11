d85e8b5dc1f9de5fd58a56dabb500d85d8fcdaf5
alien <bhupensahoo200127@gmail.com>
2026-01-11 13:15:41 +0530

    Parsing doc in worker

---
+++ workers/uploadParserWorker.js
@@ -0,0 +1,54 @@
+import { parentPort } from 'worker_threads';
+import xlsx from 'xlsx';
+
+if (!parentPort) throw new Error('This file must be run as a worker thread');
+
+parentPort.on('message', async (message) => {
+  const { arrayBuffer } = message;
+
+  try {
+    const buffer = Buffer.from(arrayBuffer);
+    const workbook = xlsx.read(buffer, { type: 'buffer' });
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+    const rows = xlsx.utils.sheet_to_json(sheet);
+
+    const normalize = rows.map((row) => {
+      const normalizedRow = {};
+      for (const key in row) {
+        if (row.hasOwnProperty(key)) {
+          const value = row[key];
+          normalizedRow[key.toLowerCase()] = value;
+        }
+      }
+
+      if (normalizedRow.policy_start_date && typeof normalizedRow.policy_start_date === 'number') {
+        normalizedRow.policy_start_date = new Date(
+          Math.round((normalizedRow.policy_start_date - 25569) * 86400 * 1000)
+        );
+      }
+      if (normalizedRow.policy_end_date && typeof normalizedRow.policy_end_date === 'number') {
+        normalizedRow.policy_end_date = new Date(
+          Math.round((normalizedRow.policy_end_date - 25569) * 86400 * 1000)
+        );
+      }
+
+      if (normalizedRow.dob && typeof normalizedRow.dob === 'number') {
+        normalizedRow.dob = new Date(
+          Math.round((normalizedRow.dob - 25569) * 86400 * 1000)
+        );
+      }
+      return normalizedRow;
+    });
+
+    parentPort.postMessage({
+      success: true,
+      rows: normalize,
+    });
+  } catch (error) {
+    parentPort.postMessage({
+      success: false,
+      error: error.message,
+    });
+  }
+})
