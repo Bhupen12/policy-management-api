fe4790bbc1171635f0524ae7d423ae5612262686
alien <bhupensahoo200127@gmail.com>
2026-01-11 11:19:57 +0530

    Database connection moved to config\database.js

--- index.js
+++ index.js
@@ -2,33 +2,25 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import mongoose from "mongoose";
+import { connectDB } from "./config/database.js"
 import router from "./router.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI;
 
 async function startApp() {
-  try {
-    await mongoose.connect(MONGO_URI);
-    console.log("Connected to MongoDB");
-    
-    app.use(express.json());
+  await connectDB();
 
-    app.get("/", (req, res) => {
-      res.send("Hello, World!");
-    });
+  app.use(express.json());
 
-    app.use("/api", router)
+  app.get("/", (req, res) => {
+    res.send("Hello, World!");
+  });
+  app.use("/api", router)
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Failed to connect to MongoDB", error);
-    process.exit(1);
-  }
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 }
 
 startApp();
