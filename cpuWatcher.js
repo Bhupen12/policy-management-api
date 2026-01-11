e91f706d78c439773472b11056ecb0a192dd5af3
alien <bhupensahoo200127@gmail.com>
2026-01-11 13:44:41 +0530

    On 70% usage cpu project will stopped

---
+++ cpuWatcher.js
@@ -0,0 +1,38 @@
+import os from 'os';
+
+const CPU_LIMIT = 70;
+
+let previousCpuInfo = os.cpus();
+
+function getCpuUsage() {
+    const currentCpuInfo = os.cpus();
+    let totalIdleDiff = 0;
+    let totalTickDiff = 0;
+
+    for (let i = 0; i < currentCpuInfo.length; i++) {
+        const prevCpu = previousCpuInfo[i];
+        const currCpu = currentCpuInfo[i];
+        const prevIdle = prevCpu.times.idle;
+        const currIdle = currCpu.times.idle;
+        const prevTick = Object.values(prevCpu.times).reduce((a, b) => a + b, 0);
+        const currTick = Object.values(currCpu.times).reduce((a, b) => a + b, 0);
+        totalIdleDiff += currIdle - prevIdle;
+        totalTickDiff += currTick - prevTick;
+    }
+
+    previousCpuInfo = currentCpuInfo;
+
+    const cpuUsage = 100 - (totalIdleDiff / totalTickDiff) * 100;
+    return cpuUsage;
+}
+
+setInterval(() => {
+    const cpuUsage = getCpuUsage();
+    console.log(`CPU Usage: ${cpuUsage}%`);
+    if (cpuUsage > CPU_LIMIT) {
+        console.warn(`Warning: CPU usage is above ${CPU_LIMIT}%!`);
+        process.exit(1);
+    }
+}, 5000);
+
+export default getCpuUsage;
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import './cpuWatcher.js';
+
 import dotenv from "dotenv";
 dotenv.config();
 
